eef9dc42eaa4dcc09bcac01e301bbfb75385c853
sgh <sugh029@163.com>
2019-05-07 15:55:03 +0800

    a demo for FastDFS_SpringBoot from sgh

--- src/test/java/com/sgh/FdfsSpringbootApplicationTests.java
+++ src/test/java/com/sgh/FdfsSpringbootApplicationTests.java
@@ -1,16 +1,47 @@
 package com.sgh;
 
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.github.tobato.fastdfs.domain.proto.storage.DownloadByteArray;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+
+import java.io.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FdfsSpringbootApplicationTests {
 
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+    // 测试上传
+    @Test
+    public void testFileUpload() throws FileNotFoundException {
+        File file = new File("D:\\水.jpg");
+        FileInputStream inputStream = new FileInputStream(file);
+        StorePath storePath = storageClient.uploadFile(inputStream,
+                file.length(), "jpg", null);
+        System.out.println(storePath.getGroup() + " | " +
+                storePath.getPath());
+    }
+
+    // 测试下载
+    @Test
+    public void testDownloadFile() throws IOException {
+        byte[] file = storageClient.downloadFile("group1", "M00/00/00/wKhfClzQ8iSAeWpgAArv1KfunAg648.jpg", new DownloadByteArray());
+        FileOutputStream fileOutputStream = new FileOutputStream("D:\\d.jpg");
+        fileOutputStream.write(file);
+        fileOutputStream.close();
+
+    }
+
     @Test
     public void contextLoads() {
+
     }
 
 }
